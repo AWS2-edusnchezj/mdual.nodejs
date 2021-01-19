dd1ec2a9904d624854d10174a844f4258c1e002f
edusnchezj <eduardsnchez@gmail.com>
2021-01-19 18:20:13 +0100

    Update 18:20 19.1.2021

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('<form><label for="username">Usuario: <input id="username" name="username" type="text" required/></label><br><br><label for="password">Contraseña: <input id="password" name="password" type="password" required/> <input type="submit"></form>')
+  res.send('<form action="/benvingut" method="POST"><label for="username">Usuario: <input id="username" name="username" type="text" required/></label><br><br><label for="password">Contraseña: <input id="password" name="password" type="password" required/> <input type="submit"></form>')
 })
+
+app.get('/benvingut', (req, res) => {
+	res.send('Benvingut, ' + req.body.username)
+	res.end()
+  })
 
 app.listen(port, () => {
   console.log('Example app listening at http://localhost:${port}')
