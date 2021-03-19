30f0803082d23fd74dd4bb430b48b5e1c7774987
edusnchezj <eduardsnchez@gmail.com>
2021-03-19 18:28:04 +0100

    Update 19.03.2021 18:28

---
+++ www/js/script.js
@@ -0,0 +1,33 @@
+function showArtists(query) {
+	if(query != '') {
+		$('#collection-progress').show();
+		$.ajax({
+			url: 'https://musicbrainz.org/ws/2/artist?query=' + query,
+			type: 'get',
+			dataType: 'JSON',
+			success: function(data) {
+				$('.collection').html('');
+				for(let artist in data['artists']) {
+					$('.collection').append(createCardArtist(data['artists'][artist]));
+				}
+			},
+			complete: function() {
+				$('#collection-progress').hide();
+			}
+		});
+	} else {
+		$('.collection').html('');
+	}
+}
+
+function createCardArtist(params) {
+	return '<li class="collection-item avatar"><img src="https://pbs.twimg.com/profile_images/1188507013233479681/WuNwaQ8R_400x400.jpg" alt="" class="circle"><span class="title">'+ params['name'] +'</span><p><em>'+ hideUndefined(params['type']) +'</em></p><a href="#details" class="secondary-content"><i class="material-icons">chevron_right</i></a></li>';
+	
+}
+
+function hideUndefined(value) {
+	if(typeof value === "undefined") {
+		return '';
+	}
+	return value;
+}
